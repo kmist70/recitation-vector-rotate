c2ab0da914778adcdd5ac57e8ed3f8c3de0fa32a
Krishna <krishna.college8829@gmail.com>
2025-09-06 23:34:11 +0000

    method 2 prototype implemented (check for compilation first)

--- src/solution.cc
+++ src/solution.cc
@@ -26,11 +26,38 @@ std::vector<int> RotateChunks(const std::vector<int>& vect, unsigned int chunk_s
   // standard solution algorithm
     // find a way to split chunks
     // go through it
-  for (unsigned int i = 0; i < result.size(); i += chunk_size) {
-    
-    // result = RotateRight(result, actual_rotations);
+  std::vector<std::vector<int>> chunk_list;
+
+  int c = 0;  //variable tracking index of 1D vector
+  for (unsigned int a = 0; a < next_chunk; ++a) {
+    for (unsigned int b = 0; b < chunk_size; ++b) {
+      chunk_list[a][b] = result[c];
+      ++c;
+    }
+  }
+  //handles extra values that dont divide evenly
+  if (c < result.size()) {
+    while (c < result.size()) {
+      int b = 0;
+      chunk_list[next_chunk][b] = result[c];
+      ++b;
+      ++c;
+    }
   }
 
+  result.clear();
+  //by this point, all values should be in chunks assigned to their own subvectors and result is cleared
+  //rotates each subvector in the 2D vector
+  for (unsigned int i = 0; i < chunk_list.size(); ++i) {
+    chunk_list[i] = RotateRight(chunk_list[i], actual_rotations);
+  }
+
+  //populates result again with the rotated chunks
+  for (unsigned int a = 0; a < chunk_list.size(); ++a) {
+    for (unsigned int b = 0; b < chunk_list[a].size(); ++b) {
+      result.push_back(chunk_list[a][b]);
+    }
+  }
 
   return result;
 }
