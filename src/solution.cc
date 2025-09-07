5bbf2c3022fe164c564b616f433f69e9ea12e9fc
Krishna <krishna.college8829@gmail.com>
2025-09-07 02:36:07 +0000

    new prototype for method 2

--- src/solution.cc
+++ src/solution.cc
@@ -13,49 +13,34 @@ std::vector<int> RotateRight(const std::vector<int>& vect, unsigned int no_rotat
 std::vector<int> RotateChunks(const std::vector<int>& vect, unsigned int chunk_size, unsigned int no_rotations) {
   std::vector<int> result = vect;
 
-  // handles unique cases
+  // handles case where chunk size is 0
   if (chunk_size == 0) {
+    result.clear();
     return result;
   }
-  unsigned int next_chunk = result.size() / chunk_size;
-  unsigned int actual_rotations = no_rotations % next_chunk;
+
+  unsigned int num_chunk = result.size() / chunk_size;
+  unsigned int actual_rotations = no_rotations % num_chunk;
+
   if (actual_rotations == 0) {
     return result;
   }
 
-  // standard solution algorithm
-    // find a way to split chunks
-    // go through it
-  std::vector<std::vector<int>> chunk_list;
-
-  int c = 0;  //variable tracking index of 1D vector
-  for (unsigned int a = 0; a < next_chunk; ++a) {
-    for (unsigned int b = 0; b < chunk_size; ++b) {
-      chunk_list[a][b] = result[c];
-      ++c;
+  for (unsigned int i = 0; i < result.size(); i += chunk_size) {
+    // for tail cases where chunk size is not the chunk size given at the beginning
+    if((i * chunk_size) + chunk_size > result.size() && result.size() % chunk_size != 0) {
+      chunk_size = result.size % chunk_size;
     }
-  }
-  //handles extra values that dont divide evenly
-  if (c < result.size()) {
-    while (c < result.size()) {
-      int b = 0;
-      chunk_list[next_chunk][b] = result[c];
-      ++b;
-      ++c;
+    
+    // standard procedure to rotate chunks
+    std::vector<int> current_chunk(chunk_size);
+    for (unsigned int j = 0; j < chunk_size; ++j) {
+      current_chunk[j] = result[(i+1) * j];
     }
-  }
+    current_chunk = RotateRight(current_chunk, actual_rotations);
 
-  result.clear();
-  //by this point, all values should be in chunks assigned to their own subvectors and result is cleared
-  //rotates each subvector in the 2D vector
-  for (unsigned int i = 0; i < chunk_list.size(); ++i) {
-    chunk_list[i] = RotateRight(chunk_list[i], actual_rotations);
-  }
-
-  //populates result again with the rotated chunks
-  for (unsigned int a = 0; a < chunk_list.size(); ++a) {
-    for (unsigned int b = 0; b < chunk_list[a].size(); ++b) {
-      result.push_back(chunk_list[a][b]);
+    for (unsigned int j = 0; j < chunk_size; ++j) {
+      result[(i+1) * j] = current_chunk[j];
     }
   }
 
