e74e696e6931cd1f1c39d150da2c9bbe4082a1f6
Krishna <krishna.college8829@gmail.com>
2025-09-07 02:59:50 +0000

    method 2 complete and needs testing

--- src/solution.cc
+++ src/solution.cc
@@ -11,36 +11,39 @@ std::vector<int> RotateRight(const std::vector<int>& vect, unsigned int no_rotat
 }
 
 std::vector<int> RotateChunks(const std::vector<int>& vect, unsigned int chunk_size, unsigned int no_rotations) {
-  std::vector<int> result = vect;
+  std::vector<int> result;
 
   // handles case where chunk size is 0
   if (chunk_size == 0) {
-    result.clear();
     return result;
   }
 
-  unsigned int num_chunk = result.size() / chunk_size;
-  unsigned int actual_rotations = no_rotations % num_chunk;
+  unsigned int actual_rotations = no_rotations % chunk_size;
 
   if (actual_rotations == 0) {
-    return result;
+    return vect;
   }
 
-  for (unsigned int i = 0; i < result.size(); i += chunk_size) {
+  for (unsigned int i = 0; i < vect.size(); i += chunk_size) {
+    unsigned int current_chunk_size = chunk_size;
+
     // for tail cases where chunk size is not the chunk size given at the beginning
-    if((i * chunk_size) + chunk_size > result.size() && result.size() % chunk_size != 0) {
-      chunk_size = result.size % chunk_size;
+    if(i + current_chunk_size > vect.size()) {
+      current_chunk_size = vect.size() - i;
     }
     
     // standard procedure to rotate chunks
-    std::vector<int> current_chunk(chunk_size);
-    for (unsigned int j = 0; j < chunk_size; ++j) {
-      current_chunk[j] = result[(i+1) * j];
+    std::vector<int> current_chunk;
+    for (unsigned int j = 0; j < current_chunk_size; ++j) {
+      current_chunk.push_back(vect[i+j]);
     }
-    current_chunk = RotateRight(current_chunk, actual_rotations);
 
-    for (unsigned int j = 0; j < chunk_size; ++j) {
-      result[(i+1) * j] = current_chunk[j];
+    // new vector for the rotated chunk
+    std::vector<int> rotated_chunk = RotateRight(current_chunk, actual_rotations);
+
+    // adds to the vector assigned to result, which holds all the rotated chunks
+    for (unsigned int j = 0; j < rotated_chunk.size(); ++j) {
+      result.push_back(rotated_chunk[j]);
     }
   }
 
